57ac9a8f0c6b2fcf1d263533505c54e12e66ab20
Justin Tarnoff <justin.tarnoff@gmail.com>
2015-10-18 17:43:14 -0500

    Initial commit

---
+++ app/src/main/java/com/jtarnoff/bgtools/NavigationButton.java
@@ -0,0 +1,19 @@
+package com.jtarnoff.bgtools;
+
+import android.content.Context;
+import android.widget.ImageButton;
+
+/**
+ * Created by jtarnoff on 10/17/2015.
+ */
+public class NavigationButton extends ImageButton {
+
+    String buttonText = "";
+
+    public NavigationButton(Context context) {
+        super(context);
+    }
+
+    
+
+}
